fe50914d5e506f0296aa3407d5603d91929b37ed
HotHyun <ohsong656565@gmail.com>
2024-04-13 14:33:07 +0900

    feat: add HotHyun's Image
    - HotHyun's Image & introduce add
    - aksed -> asked

--- cyfonii-react/src/assets/fake-data/data-testimonials2.js
+++ cyfonii-react/src/assets/fake-data/data-testimonials2.js
@@ -1,37 +1,33 @@
-
-import icon1 from '../images/icon/quote-2.png'
-import avt1 from '../images/layouts/team-10.jpg'
-import avt2 from '../images/layouts/avt-09.png'
-import avt3 from '../images/layouts/avt-10.png'
-
+import icon1 from "../images/icon/quote-2.png";
+import avt1 from "../images/layouts/team-10.jpg";
+import avt2 from "../images/layouts/team-11.jpg";
+import avt3 from "../images/layouts/avt-10.png";
 
 const dataTestimonials2 = [
-
-    {
-        id: 1,
-        icon: icon1,
-        text: '"When You\'re Going To Change The World, Don\'t Ask For Permission" 작은 아이디어와 시도들이 모여 세상을 변화시킬 수 있다고 생각합니다. 오늘 우리의 도전은 그 변화의 첫 걸음이 될것입니다.',
-        avt: avt1,
-        name: 'Shin yeongbin',
-        position: 'CEO & AI Developer'
-    },
-    {
-        id: 2,
-        icon: icon1,
-        text: '“ Success is not final, failure is not fatal: It is the courage to continue that counts." - Winston Churchill. “',
-        avt: avt2,
-        name: 'Shin sanghyun',
-        position: 'CTO & Frontend Developer'
-    },
-    {
-        id: 3,
-        icon: icon1,
-        text: '“ The only way to do great work is to love what you do. “',
-        avt: avt3,
-        name: 'Kim minseung',
-        position: 'Server Developer'
-    },
-
-]
+  {
+    id: 1,
+    icon: icon1,
+    text: "\"When You're Going To Change The World, Don't Ask For Permission\" 작은 아이디어와 시도들이 모여 세상을 변화시킬 수 있다고 생각합니다. 오늘 우리의 도전은 그 변화의 첫 걸음이 될것입니다.",
+    avt: avt1,
+    name: "Shin yeongbin",
+    position: "CEO & AI Developer",
+  },
+  {
+    id: 2,
+    icon: icon1,
+    text: "나만이 줄 수 있는 선한 영향력을 주고 싶어하는, 사회라는 물결에 휩쓸리지 않고 나만의 물결을 만들어가고자 하는, 원활한 커뮤니케이션을 위해 타 분야에 대한 경험을 주저하지 않는",
+    avt: avt2,
+    name: "Shin sanghyun",
+    position: "CTO & Frontend Developer",
+  },
+  {
+    id: 3,
+    icon: icon1,
+    text: "“ The only way to do great work is to love what you do. “",
+    avt: avt3,
+    name: "Kim minseung",
+    position: "Server Developer",
+  },
+];
 
 export default dataTestimonials2;
